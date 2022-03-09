34bcca619005babe1732ae7cc8f222e793b868ec
wildething96 <85587609+wildething96@users.noreply.github.com>
2022-03-09 12:40:17 +0000

    Update userRoutes.js

--- src/User/userRoutes.js
+++ src/User/userRoutes.js
@@ -3,7 +3,7 @@ const { hashPassword } = require("../middleware");
 const { addUser, listUsers, updateUser, deleteUser, login } = require("./userMethod");
 const userRouter = Router();
 
-userRouter.post("/user", hashPassword, addUser);
+userRouter.post("/User", hashPassword, addUser);
 userRouter.get("/User", listUsers);
 userRouter.put("/User", updateUser);
 userRouter.delete("/User", deleteUser);
