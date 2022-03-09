017146dcd8a139f9f4cbee330cb25e6909b4dad0
wildething96 <85587609+wildething96@users.noreply.github.com>
2022-03-09 14:42:57 +0000

    Update userMethod.js

--- src/User/userMethod.js
+++ src/User/userMethod.js
@@ -9,8 +9,7 @@ exports.addUser = async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).send({ err: error.message });
-  }
-=======
+ }
 
 exports.addUser = async (userObj) => {
     try{
